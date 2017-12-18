7871cc9ab1063fa3700bcab55b3b9c658aa4a5b9
Matthew Ford <matthewford@Matthews-MacBook-Pro.local>
2017-12-18 14:51:11 -0800

    install primeng and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ButtonModule, DialogModule } from "primeng/primeng";
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
